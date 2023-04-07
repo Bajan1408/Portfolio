2bc00990e07c32d91a3e0fbe4c1333fde37c7bcd
Bajan1408 <gianrodrigues@gmail.com>
2023-04-07 00:36:51 -0300

    Atualizando hard skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,6 @@ function updataSoftSkills (profileData) {
 
 (async () => {
     const profileData = await fetchProfileData();
-    console.log(profileData);
 
     updateProfileInfo(profileData);
     updataSoftSkills(profileData);
