ea2ba27644c46e38e6180269ed2be9259c7629f3
Bajan1408 <gianrodrigues@gmail.com>
2023-04-06 23:07:11 -0300

    Iniciando a alimentação dinàmica dos dados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,29 @@
 
+function updateProfileInfo (profileData) {
+    /*const photo = document.getElementById('profile.photo');
+    photo.src = profileData.photo;
+    photo.alt = profileData.name; */
+
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name;
+
+    const job = document.getElementById('profile.job');
+    job.innerText =  profileData.job;
+
+    const location = document.getElementById('profile.location');
+    location.innerText = profileData.location;
+
+    const phone = document.getElementById('profile.phone');
+    phone.innerText = `tel: ${profileData.phone}`;
+
+    const mail = document.getElementById('profile.mail');
+    name.innerText = `mailto: ${profileData.mail}`;
+
+}
+
 (async () => {
     const profileData = await fetchProfileData();
     console.log(profileData);
 
+    updateProfileInfo(profileData);
 })()
