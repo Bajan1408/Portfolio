60c6be79c3d28e14b37e2bc58f16f094e70de447
Bajan1408 <gianrodrigues@gmail.com>
2023-04-13 00:48:08 -0300

    Mais correções

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ function updatePortfolio (profileData) {
 
 function updateExperience (profileData) {
     const experience = document.getElementById('profile.profExp.name');
-    experience.innerHTML = profileData.experiencias.map((exp) => `
+    experience.innerHTML = profileData.experience.map((exp) => `
         <li>
            <h3 class="experience">${exp.nome}</h3>
            <p class="period">${exp.periodo}</p>
@@ -64,5 +64,5 @@ function updateExperience (profileData) {
     updatePortfolio(profileData); 
     updateExperience(profileData); 
 
-})()
+})();
 
